22284fab40741f4b6f4b92127e41f2232956d467
Jan Alonzo <jmalonzo@taguchimail.com>
2013-11-10 00:48:15 +1100

    (Golang) Problem 7 using big.Int

---
+++ go/7.go
@@ -0,0 +1,24 @@
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func isPrime(x *big.Int) bool {
+	// FIXME implement primality test here. No cheating :-)
+	return x.ProbablyPrime(10) == true
+}
+
+func main() {
+	p := big.NewInt(0)
+	current := 0
+	for current < 10001 {
+		p = p.Add(p, big.NewInt(1))
+		if isPrime(p) == true {
+			current += 1
+		}
+	}
+	fmt.Println("The 10 001st prime number is", p)
+}
